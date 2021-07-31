5498306ed8797912b4e914b77e2ff0747914c66c
Sven Buettner <sbuettner@nosgroup.de>
2021-07-31 21:22:40 +0200

    config.json wird entfernt. WIrd nun als Prop an App von index.js übergeben

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import {Navigation} from './Navigation.js';
 import {Content} from './Content.js';
 import {Footer} from './Footer.js';
-import config from '../config/config.json';
+
+
+
 
 export class App extends React.Component {
   render() {
     return (
       <div className='App'>
-          <Navigation config={config[0].navigation}/>
-          <Content config={config[0].content}/>
-          <Footer config={config[0].footer}/>
+          <Navigation config={this.props.config.navigation}/>
+          <Content config={this.props.config.content}/>
+          <Footer config={this.props.config.footer}/>
       </div>
     );
   }
