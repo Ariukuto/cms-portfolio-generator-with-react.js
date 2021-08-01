72a8029db698bb84ce9af4a54bd6b64564a820cd
Sven Buettner <sbuettner@nosgroup.de>
2021-08-02 01:24:26 +0200

    icon kann nun auch Bilder rendern, wenn is_icon in der config.json auf false steht.

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -5,17 +5,44 @@ export class Icon extends React.Component {
         constructor(props) {
             super(props);
             this.state = {
-                icon: props.icon,
+                image: null
+            }
+            let data = props.icon;
+            let is_icon = data.is_icon;
+            this.name = data.name;
+            this.color = data.color;
+            this.size = data.size;
+            this.abstand_unten = data.abstand_unten;
+            
+            if(is_icon === false) {
+                import(`../imgs/${this.name}`).then(img => {
+                    console.log(img);
+                    this.setState({image: img.default})
+                });
             }
         }
 
         render() {
+            if(this.state.image) {
+                return(
+                    <div className='Icon'>
+                        <img 
+                            src={this.state.image} alt={this.name}
+                            style={{
+                                height: this.size,
+                                padding:"2rem",
+                                marginBottom: this.abstand_unten,
+                            }}
+                        />
+                    </div>
+                )
+            }
             return(
                 <div className='Icon'>
-                    <i className={this.state.icon.name}
+                    <i className={this.name}
                         style={{
-                            color: this.state.icon.color,
-                            fontSize: this.state.icon.size
+                            color: this.color,
+                            fontSize: this.size
                         }}
                     />
                 </div>    
