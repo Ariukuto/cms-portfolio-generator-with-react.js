1764cdeb358df19a0a3a6af6c97efc7d3840ba7f
Sven Buettner <sbuettner@nosgroup.de>
2021-08-02 17:45:55 +0200

    Imagebox und Textbox durch ImgTextRow Komponente ersetzt

--- src/components/Vorstellung.js
+++ src/components/Vorstellung.js
@@ -1,30 +1,22 @@
 import React from 'react';
-import {Headline} from './Headline.js';
-import {Imagebox} from './Imagebox';
-import {Textbox} from './Textbox';
+import {Headline} from './Headline';
+import {ImgTextRow} from './ImgTextRow';
 
 
 export class Vorstellung extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            headline: this.props.data.headline,
-            textbox: this.props.data.textbox,
-            imgbox: this.props.data.imgbox,
-        }
+        this.headline = props.data.headline;
+        this.textbox = props.data.textbox;
+        this.imgbox = props.data.imgbox;
     }
 
     render() {
         return(
-            <div className="Vorstellung container">
-                <Headline text={this.state.headline}/>
-                <div className='d-flex justify-content-center'>
-                    <div className='d-flex flex-wrap'>
-                        <Imagebox imgbox={this.state.imgbox} />
-                        <Textbox textbox={this.state.textbox} />
-                    </div>
-                </div>
+            <div className="Vorstellung container-fluid">
+                <Headline text={this.headline}/>
+                <ImgTextRow  imgbox={this.imgbox} textbox={this.textbox} />
             </div>
         );
     }
