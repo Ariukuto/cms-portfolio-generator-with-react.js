fcaa37cc161c7f205a61cd0780145419b4caa95e
Sven Buettner <sbuettner@nosgroup.de>
2021-08-02 18:39:30 +0200

    Hintergrundfarbe und Hintergrundbild der Seite lässt sich nun über die config.json anpassen

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,32 @@ import {Footer} from './Footer.js';
 
 
 export class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      backgroundcolor: props.config.page.background.backgroundcolor,
+      backgroundimgRender: props.config.page.background.img.render,
+      imgname: props.config.page.background.img.name,
+      src: null,
+    }
+    if(this.state.backgroundimgRender === true) {
+      import(`../imgs/${this.state.imgname}`).then(image => {
+          this.setState({
+              src:image.default
+           });
+      });
+  }
+  }
+
   render() {
     return (
-      <div className='App'>
+      <div className='App' 
+        style={{
+          backgroundColor:this.state.backgroundcolor,
+          backgroundImage:`url(${this.state.src})`,
+        }}
+      >
           <Navigation config={this.props.config.navigation}/>
           <Content config={this.props.config.content}/>
           <Footer config={this.props.config.footer}/>
