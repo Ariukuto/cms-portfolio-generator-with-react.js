a1d62e4fe6668c6edbaf8f2dba4a8efe6f68b2b2
Sven Buettner <sbuettner@nosgroup.de>
2021-08-02 17:15:41 +0200

    Refactoring

--- src/components/Details.js
+++ src/components/Details.js
@@ -6,26 +6,24 @@ export class Details extends React.Component
 {
     constructor(props) {
         super(props);
-        this.state = {
-            config: this.props.data
-        }
+        this.config = this.props.data;
     }
 
     render() {
         return(
-            <div className='Details'>
-                <Headline text={this.state.config.headline} />
-                {this.state.config.content.map((content, index) => {
+            <div className={`Details ${this.config.fullwith === "true" ? 'container-fluid' : 'container'}`}>
+                <Headline text={this.config.headline} />
+                {this.config.rows.map((row, index) => {
                     let rest = index % 2;
                     if(rest === 0) {
                         return(
                             <div key={index} className='row'>
                                 <div className='col-sm column-picture'>
-                                    <Image img={content.img} />
+                                    <Image img={row.imgbox.img} />
                                 </div>
                                 <div className='col-sm column-text'>
-                                    <h4>{content.headline}</h4>
-                                    {content.text}
+                                    <h4>{row.headline}</h4>
+                                    {row.text}
                                 </div>
                             </div>
                         )
@@ -33,16 +31,15 @@ export class Details extends React.Component
                         return(
                             <div key={index} className='row'>
                                 <div className='col-sm column-text'>
-                                    <h4>{content.headline}</h4>
-                                    {content.text}
+                                    <h4>{row.headline}</h4>
+                                    {row.text}
                                 </div>
                                 <div className='col-sm column-picture'>
-                                    <Image img={content.img} /> 
+                                    <Image img={row.imgbox.img} /> 
                                 </div>
                             </div>
                         ) 
                     }
-
                 })}
             </div>
         )
