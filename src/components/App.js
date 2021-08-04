7a754161ca06e605a8422ac5eed91a8f12d9b733
Sven Buettner <sbuettner@nosgroup.de>
2021-08-05 00:16:04 +0200

    Refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -8,33 +8,41 @@ export class App extends React.Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
       backgroundcolor: props.config.page.background.backgroundcolor,
       backgroundimgRender: props.config.page.background.img.render,
       imgname: props.config.page.background.img.name,
       src: null,
     }
+
     if(this.state.backgroundimgRender === true) {
       import(`../imgs/${this.state.imgname}`).then(image => {
           this.setState({
               src:image.default
            });
       });
-  }
+    }
+
+    this.style = {
+      backgroundColor: this.state.backgroundcolor,
+      backgroundImage: `url(${this.state.src})`
+    }
+
   }
 
   render() {
     return (
-      <div className='App' 
-        style={{
-          backgroundColor:this.state.backgroundcolor,
-          backgroundImage:`url(${this.state.src})`,
-        }}
-      >
+      <div className='App' style={this.style}>
+
           <Navigation config={this.props.config.navigation}/>
+          
           <Content config={this.props.config.content}/>
+
           <Footer config={this.props.config.footer}/>
+
       </div>
+
     );
   }
 }
