1895a05f4c9bdf2ad427a1fffc448a02dab05347
Sven Buettner <sbuettner@nosgroup.de>
2021-08-12 19:58:03 +0200

    changed Imagebox to NewImage Component

--- src/components/Vorstellung.js
+++ src/components/Vorstellung.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Headline} from './Headline';
 
 import {Imagebox} from './Imagebox';
+
+import { NewImage } from './NewImage';
 import {Textbox} from './Textbox';
 
 
@@ -16,13 +18,14 @@ export class Vorstellung extends React.Component {
             'fullwith': props.data.fullwith,
             'textbox': props.data.textbox,
             'imgbox':props.data.imgbox,
+            'image': props.data.image,
         }
     }
 
      
 
     render() {
-        let style = {backgroundcolor: this.state.backgroundcolor};
+        let style = {backgroundColor: this.state.backgroundcolor};
         let layout = this.state.fullwith === true ? 'container-fluid' : 'container';
         let shadow = this.state.shadow === true ? 'shadow' : '';
 
@@ -30,7 +33,8 @@ export class Vorstellung extends React.Component {
             <div className={`Vorstellung pb-5 ${layout} ${shadow}`} style={style}>
                 <Headline text={this.state.headline}/>
                 <div className='d-flex flex-wrap justify-content-center'>
-                    <Imagebox imgbox={this.state.imgbox} />
+                    {/*<Imagebox imgbox={this.state.imgbox} />*/}
+                    <NewImage data={this.state.image} />
                     <Textbox textbox={this.state.textbox} />
                 </div>
             </div>
