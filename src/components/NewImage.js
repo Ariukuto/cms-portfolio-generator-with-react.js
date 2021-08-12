0d2d57a9b5253d05f51209c55c63c9d45b5898c8
Sven Buettner <sbuettner@nosgroup.de>
2021-08-12 19:25:35 +0200

    added shadow, marginBottom and marginTop as state attribute

--- src/components/NewImage.js
+++ src/components/NewImage.js
@@ -6,20 +6,28 @@ export class NewImage extends React.Component
         super(props);
         this.state = {
             mobile: {
+                src: "",
                 imageName: "",   
                 height: "",
                 width: "",
-                src: "",
+                borderRadius: "",
+                shadow: "",
+                DistanceUp: "",
+                DistanceDown: "",                
             },
             widescreen: {
+                src: "",
                 imageName: "",      
                 height: "",
                 width: "",
-                src: "",
+                borderRadius: "",
+                shadow: "",
+                DistanceUp: "",
+                DistanceDown: "",
             }
         }
 
-        if(this.props.data.mobile.imageName !== "") {
+        if(this.props.data.mobile && this.props.data.mobile.imageName) {
             import(`../imgs/${this.props.data.mobile.imageName}`).then(image => {
                 this.setState({
                     mobile : {
@@ -28,12 +36,15 @@ export class NewImage extends React.Component
                         height: props.data.mobile.height,
                         width: props.data.mobile.width,
                         borderRadius: props.data.mobile.borderRadius,
+                        shadow: props.data.mobile.shadow,
+                        DistanceUp: props.data.mobile.DistanceUp,
+                        DistanceDown: props.data.mobile.DistanceDown,  
                     }
                  });
             });
         }
        
-        if(this.props.data.widescreen.imageName !== "") {
+        if(this.props.data.widescreen && this.props.data.widescreen.imageName) {
             import(`../imgs/${this.props.data.widescreen.imageName}`).then(image => {
                 this.setState({
                     widescreen : {
@@ -42,6 +53,9 @@ export class NewImage extends React.Component
                         height: props.data.widescreen.height,
                         width: props.data.widescreen.width,
                         borderRadius: props.data.widescreen.borderRadius,
+                        shadow: props.data.widescreen.shadow,
+                        DistanceUp: props.data.widescreen.DistanceUp,
+                        DistanceDown: props.data.widescreen.DistanceDown,  
                     }
                  });
             });
@@ -51,22 +65,26 @@ export class NewImage extends React.Component
     render() {
         return(
             <div className="NewImage">
-                <img className="show_only_on_widescreen"
+                <img className={`show_only_on_widescreen ${this.state.mobile.shadow ? 'shadow' : ''}`}
                     src={this.state.widescreen.src} 
                     alt={this.state.widescreen.imageName}
                     style={{
                         width: this.state.widescreen.width,
                         height:this.state.widescreen.height,
                         borderRadius: this.state.widescreen.borderRadius,
+                        marginTop: this.state.widescreen.DistanceUp,
+                        marginBottom: this.state.widescreen.DistanceDown,
                     }} 
                 />
-                <img className="show_only_on_mobile"
+                <img className={`show_only_on_mobile ${this.state.widescreen.shadow ? 'shadow' : ''}`}
                     src={this.state.mobile.src} 
                     alt={this.state.mobile.imageName}
                     style={{
                         width: this.state.mobile.width,
                         height:this.state.mobile.height,
                         borderRadius: this.state.widescreen.borderRadius,
+                        marginTop: this.state.mobile.DistanceUp,
+                        marginBottom: this.state.mobile.DistanceDown,
                     }} 
                 />
             </div>   
