4b9bc9824818eab5bb1bc91281d62d4a18da09db
Sven Buettner <sbuettner@nosgroup.de>
2021-08-21 13:03:02 +0200

    update

--- src/components/Window.js
+++ src/components/Window.js
@@ -45,22 +45,3 @@ export class Window extends React.Component {
       );
   }
 }
-/*
-
-          <div className={`modal fade ${this.state.MsgObject.component ? 'show' : ''}`} id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">  {this.state.MsgObject.title} </h5>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                {this.state.MsgObject.component && <DynComponent ComponentName={this.state.MsgObject.component} />}
-              </div>
-              <div class="modal-footer">
-
-                </div>
-                </div>
-              </div>
-            </div>
-*/
