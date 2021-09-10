be566b5bd124f5b563e511d732bb1c23cb7713d5
Sven Buettner <sbuettner@nosgroup.de>
2021-09-10 16:19:04 +0200

    component improved

--- src/components/windowComponents/Licenses.js
+++ src/components/windowComponents/Licenses.js
@@ -1,19 +1,30 @@
+import React from 'react';
 import config from '../../config/lizenzen.config.json';
 import {Image} from '../Image';
 
 const Licenses = (props) => {
-
-    const list = config;
-
     return (
         <div className="licenses-component pb-2">
 
-            {list.map((obj, i) => {
-                console.log("obj", obj);
+            {config.map((obj, i) => {
+
+                const imgobj = {
+                    mobile: {
+                        imageName: obj.imageName,
+                        height: "50px",
+                        width: "50px",
+                    },
+                    widescreen: {
+                        imageName: obj.imageName,
+                        height: "50px",
+                        width: "50px",
+                    }
+                }
+
                 return (
                     <div key={i} className="row mb-4">
                         <div className="col-3 col-sm">
-                            <Image img={obj.img} height="50px" width="50px"/>
+                            <Image data={imgobj} />
                         </div>
                         <div className="col-sm d-flex">
                             <span className="d-flex align-content-center"
