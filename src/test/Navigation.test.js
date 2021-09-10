41ba8df64496ea198d4f3de164bdbaf4306f70ea
Sven Buettner <sbuettner@nosgroup.de>
2021-09-10 16:17:03 +0200

    test Improved

--- src/test/Navigation.test.js
+++ src/test/Navigation.test.js
@@ -1,7 +1,6 @@
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 import {Navigation} from '../components/Navigation';
-import config from '../config/navigation.config.json';
 
 let container = null;
 
@@ -18,17 +17,110 @@ afterEach(() => {
     container = null;
 });
 
-test('Render Navigation Links', () => {
+const config = {
+    "fullwith": true,
+    "overBanner": true,
+    "navbarAlign": "right",
+    "backgroundcolor": "#f9fafb59",
+    "color": "",
+    "logo": {   
+        "widescreen": {
+            "imageName": "profil.png",
+            "height": "50px",
+            "width": "50px",
+            "borderRadius": "100%",
+            "shadow": false,
+            "DistanceUp": "",
+            "DistanceDown": ""
+        } ,   
+        "mobile": {
+            "imageName": "profil.png",
+            "height": "50px",
+            "width": "50px",
+            "borderRadius": "100%",
+            "shadow": false,
+            "DistanceUp": "",
+            "DistanceDown": ""
+        }
+    },
+    "links": [
+        {"name": "linkone", "url": "https://test.de/"},
+    ]
+}
 
-    act(() => {
+
+describe('Render Navigation', () => {
+
+    test('find Nav Element', () => {
         render(<Navigation data={config} />, container);
+        const navigationElement = container.querySelector(".Ǹavigation");
+        expect(navigationElement).not.toBeFalsy();
     });
 
-    config.links.map((link, index) => {
-        let navLinkElement = container.querySelector("#a"+index);
-        let navLinkText = navLinkElement.textContent;
-        let navLinkHref = navLinkElement.getAttribute("href");
-        expect(navLinkText).toEqual(link.name);
-        expect(navLinkHref).toEqual(link.url);    
+    test('find Navigation classes', () => {
+        render(<Navigation data={config} />, container);
+        const navigationElement = container.querySelector(".Ǹavigation.absolute");
+        const layout = navigationElement.querySelector(".container-fluid");
+        expect(navigationElement).not.toBeFalsy();
+        expect(layout).not.toBeFalsy();
     });
+
+    test("find backgroundcolor", () => {
+        render(<Navigation data={config} />, container);
+        const navigationElement = container.querySelector(".Ǹavigation");
+        const backgroundcolor =  navigationElement.style.backgroundColor;
+        expect(backgroundcolor).not.toBeFalsy();
+    });
+
+    test('render logo', () => {
+        render(<Navigation data={config} />, container);
+        const navigationElement = container.querySelector(".Ǹavigation");
+        const logo = navigationElement.querySelector(".logo");
+        const logoimage = logo.querySelector("img");
+        expect(logoimage).not.toBeFalsy();      
+    });
+
+    test("check navbarAlign", () => {
+        render(<Navigation data={config} />, container);
+        const navigationElement = container.querySelector(".Ǹavigation");
+        const navbarContainer = navigationElement.querySelector("#navbarSupportedContent");
+        const justifyContent = navbarContainer.style.justifyContent;
+        expect(justifyContent).toBe("flex-end");
+    });
+
+
+    test('find navLink', () => {
+        render(<Navigation data={config} />, container);
+        let navLinkElement;
+        config.links.map((link, i) => {
+            navLinkElement = container.querySelector("#a"+i);
+        })
+        expect(navLinkElement).not.toBeFalsy()
+    });
+
+    test('find true navlink Text', () => {
+        render(<Navigation data={config} />, container);
+        let navLinkElement;
+        config.links.map((link, i) => {
+            navLinkElement = container.querySelector("#a"+i);
+            let name = navLinkElement.textContent;
+            expect(name).toBe(link.name)
+        })
+    })
+
+    test('find true navlink href', () => {
+        render(<Navigation data={config} />, container);
+        let navLinkElement;
+        config.links.map((link, i) => {
+            navLinkElement = container.querySelector("#a"+i);
+            let href = navLinkElement.href;
+            expect(href).toBe(link.url)
+        })
+    })
+
+  
+
+    
+
+
 });
