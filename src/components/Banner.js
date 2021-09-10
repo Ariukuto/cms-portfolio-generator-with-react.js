5f7c680ad0fb75da6ba149a7e09afed942aa9d69
Sven Buettner <sbuettner@nosgroup.de>
2021-09-10 19:58:44 +0200

    refactored

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import React ,{ useState } from 'react';
 import {Image} from './Image';
 
 const Banner = (props) => {
 
-    const [fullwith, setFullwith] = useState(props.data.fullwith);
-    const [height, setheight] = useState(props.data.height);
-    const [image, setImage] = useState(props.data.image);
+    const [fullwith] = useState(props.data.fullwith);
+    const [height] = useState(props.data.height);
+    const [image] = useState(props.data.image);
 
     return(
         <div className={`Banner ${fullwith ? '' : 'container'}`} style={{height: height}}>
