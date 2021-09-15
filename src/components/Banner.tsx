376b0803799b7b81a09613455479bb68d16840a6
Sven Buettner <sbuettner@nosgroup.de>
2021-09-15 20:12:30 +0200

    Banner Component changed to tsx for typescript

---
+++ src/components/Banner.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Image, ImageData} from './Image';
+
+type BannerData = {
+    fullwith: boolean,
+    height: string,
+    image: ImageData,
+}
+export type {BannerData}
+
+type BannerProps = {
+    data: BannerData
+}
+
+const Banner = ({data}:BannerProps) => {
+
+    const fullwith = data.fullwith;
+    const height = data.height;
+    const image = data.image;
+
+    return(
+        <div className={`Banner ${fullwith ? '' : 'container'}`} style={{height: height}}>
+            <Image data={image} />
+        </div> 
+    );
+    
+}
+
+export {Banner};
