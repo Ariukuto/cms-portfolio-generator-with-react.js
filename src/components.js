9dbd753cb71f42e2d0449b854ada8aab3abbee47
Ariukuto <sven.buettner93@gmx.de>
2022-04-08 23:26:27 +0200

    Datei erstellt, welche alle componenten enthält, die keine page sind

---
+++ src/components.js
@@ -0,0 +1,17 @@
+import { Banner }       from './components/Banner';
+import { Profile }  from './components/Profile';
+import { Iconrow }      from './components/Iconrow';
+import { Details }      from './components/Details';
+import { Textbox } from './components/Textbox';
+import { Headline } from './components/Headline';
+import { Collection } from './components/Collection';
+
+export const components = {
+    Banner,
+    Profile,
+    Iconrow,
+    Details,
+    Textbox,
+    Headline,
+    Collection,
+}
