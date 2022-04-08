5bfe23fb2e98bfbee4224702bd23cccb54c9371c
Ariukuto <sven.buettner93@gmx.de>
2022-04-09 00:41:23 +0200

    App.tsx in components ausgelagert

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { render } from "react-dom";
-import { App } from './components/app/App';
+import { App } from './components/App';
 import './scss/all.scss';
 
 const rootElement = document.getElementById("root");
