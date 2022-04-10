dcd62162b2dd3399103203012e95d786d7c5c031
Ariukuto <sven.buettner93@gmx.de>
2022-04-11 01:13:05 +0200

    Collection Component FlipcardCollection entwickelt

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -1,4 +1,6 @@
 import React, {useState} from "react";
+import { FlipCard } from "./flipcard";
+
 
 type CollectionProps = {
     data: {
@@ -22,7 +24,7 @@ const Collection = ({data}:CollectionProps) => {
             <div className="Collection">
                 {items.map(({headline, headlinecolor ,text, textcolor, img, backgroundcolor, hoverbgcolor}, index) => {
                     return(
-                        <div key={index} className={`collection-item shadow`} style={{backgroundColor:backgroundcolor}}>
+                        <div key={index} className={`collection-item shadow`} style={{"backgroundColor":backgroundcolor}}>
                             <img src={`./imgs/${img}`} alt={img}/>
                             <div className="text-container" style={{backgroundColor:hoverbgcolor, color:textcolor}}>
                                 <h3 className="headline"> {headline} </h3>
@@ -35,5 +37,39 @@ const Collection = ({data}:CollectionProps) => {
         </div>
     )
 }
+export const FlipCardCollection = ({data}:CollectionProps) => {
+    return (
+        <div className="container">
+            <div className="FlipCardCollection">
+                {data.items.map(({headline, headlinecolor ,text, textcolor, img, backgroundcolor, hoverbgcolor}, index) => {
+                     return (
+                        <div key={index} className={`collection-item`}>
+                            <FlipCard>
+                                <FlipCard.Frontside>
+                                    <div className="d-flex justify-content-center align-items-center bg-light border shadow" style={{background:backgroundcolor}}> 
+                                        <img src={`./imgs/${img}`} alt={img}/>
+                                    </div>
+                                </FlipCard.Frontside>
+                                <FlipCard.Backside>
+                                    <div className="d-flex flex-column justify-content-around align-items-center border shadow p-1" style={{background:hoverbgcolor}}>
+                                        <h3 className="headline" style={{color:headlinecolor}}> 
+                                            {headline} 
+                                        </h3>
+                                        <span className="text" style={{color:textcolor}}>
+                                            {text}
+                                        </span>
+                                        <img src={`./imgs/${img}`} alt={img}/>
+                                    </div>                    
+                                </FlipCard.Backside>
+                            </FlipCard>
+                        </div>
+                     )
+                })}
+            </div>
+        </div>
+    )
+}
 
-export {Collection}
+export {
+    Collection
+}
